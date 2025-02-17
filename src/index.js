0e802731c22a0ff333ac164fc15e82c3e0da8376
Markus1515 <markus.rautasalo@gmail.com>
2025-02-17 16:26:19 +0200

    kommentti positettu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import MyCounter from './mycounter';
 import { Otsikko } from './sivu';
 import { Page } from './page';
 import { PowerBI } from './visualisoinnit';
